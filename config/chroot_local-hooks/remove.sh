79f4b373ec262775d4daeddd18f15d7ad99b1044
Jun NOGATA <nogajun@gmail.com>
2008-12-14 07:49:52 +0900

    Add Makefile and config/ dir.

---
+++ config/chroot_local-hooks/remove.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# stop daemon 
+DAEMON="rsyslog"
+for i in $DAEMON; do /usr/sbin/update-rc.d -f $i remove; done
+
+# disable console
+sed -i 's/^[456]/$&/' /etc/inittab
+
+# parallel init script
+sed -i 's/^CONCURRENCY=none/CONCURRENCY=shell/' /etc/init.d/rc
+apt-get -y install insserv
+update-bootsystem-insserv
+
+# install ophcrack
+aptitude update
+aptitude -y -t experimental install ophcrack ophcrack-cli
+
+# purge packages
+apt-get -y autoremove
+
