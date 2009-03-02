10197f307d3f9c2060122ad51bdf7a7135176d25
Jun NOGATA <nogajun@gmail.com>
2009-03-02 15:28:21 +0900

    add live-helper snapshot apt-line

--- config/chroot_local-hooks/remove.sh
+++ config/chroot_local-hooks/remove.sh
@@ -4,14 +4,14 @@ set -e
 # disable console tty4,tty5,tty6
 sed -i 's/^[456]/$&/' /etc/inittab
 
-# remove daemon
-DAEMON="stop-bootlogd cron portmap rsyslog networking"
-for i in $DAEMON; do /usr/sbin/update-rc.d -f $i remove; done
-
 # parallel init script
 sed -i 's/^CONCURRENCY=none/CONCURRENCY=shell/' /etc/init.d/rc
 apt-get -y install insserv
 update-bootsystem-insserv
+
+# remove daemon
+DAEMON="stop-bootlogd cron portmap rsyslog"
+for i in $DAEMON; do /usr/sbin/update-rc.d -f $i remove; done
 
 # install ophcrack
 #aptitude update
