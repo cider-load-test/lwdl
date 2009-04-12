0e195b698985f91fa92e194a5ca6aa689655ec6c
Jun NOGATA <nogajun@gmail.com>
2009-04-12 18:01:57 +0900

    change a lot
     * split package lists
     * split restricted package repository

--- config/chroot_local-hooks/00-backports_install.sh
+++ config/chroot_local-hooks/00-backports_install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 set -e
 
-# package install
-PACKAGES="wicd openoffice.org"
+# install package 
+PACKAGES="wicd openoffice.org openoffice.orgi-help-ja openoffice.org-l10n-ja"
 apt-get -y -t lenny-backports install ${PACKAGES}
 
-# non-free package
+# install non-free packages
 apt-get -y -t lenny-backports install flashplugin-nonfree
+
+# install restricted packages
+apt-get -y -t lenny-backports install opera skype
+sed -i 's/kochi/vl/' /usr/share/opera/ini/font.ini
 
 # clean up
 apt-get -y purge lxnm
-apt-get autoremove
