623dce3d3b98f54a3c1c4ac6c3bab360e099e806
Jun NOGATA <nogajun@gmail.com>
2009-04-15 15:06:48 +0900

    remove lxlauncher and fix create icons script

--- config/chroot_local-hooks/01-create_desktop_icons.sh
+++ config/chroot_local-hooks/01-create_desktop_icons.sh
@@ -5,4 +5,4 @@ SKEL_DIR=/etc/skel/Desktop
 ICON_DIR=/usr/share/applications
 ICONS="AdobeReader.desktop evince.desktop f-spot.desktop filezilla.desktop inkscape.desktop lxde-x-www-browser.desktop ooo-startcenter.desktop opera.desktop pidgin.desktop rhythmbox.desktop skype.desktop system-config-printer.desktop totem.desktop tsclient.desktop vlc.desktop"
 
-for i in ${ICONS}; do [ -f ${i} ] && cp ${ICON_DIR}/${i} ${SKEL_DIR}/ ; done
+for i in ${ICONS}; do [ -f ${ICON_DIR}/${i} ] && cp ${ICON_DIR}/${i} ${SKEL_DIR}/ ; done
